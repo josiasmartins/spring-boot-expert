21bbd98aac266974b7438a69ff8237d5b8de1669
Caitano <josias.m.caitano@accenture.com>
2023-08-20 11:47:00 -0300

    feat: put mapping - atualizando um cliente

--- src/main/java/io/github/josiasmartins/rest/controller/ClienteController.java
+++ src/main/java/io/github/josiasmartins/rest/controller/ClienteController.java
@@ -74,5 +74,16 @@ public class ClienteController {
         return ResponseEntity.notFound().build();
     }
 
+    @PutMapping("/api/clientes/{id}")
+    @ResponseBody // saida
+    public ResponseEntity update(@PathVariable Integer id, @RequestBody Cliente cliente) {
+        return clientes.findById(id)
+                .map(clienteExistente -> {
+                    cliente.setId(clienteExistente.getId());
+                    clientes.save(cliente);
+                    return ResponseEntity.noContent().build();
+                }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
 
 }
