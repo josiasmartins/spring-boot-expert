9e46a0bcfe5c631300348f036f733377e5253b4e
Caitano <josias.m.caitano@accenture.com>
2023-08-19 22:22:34 -0300

    feat: mapeamento a entidade produto

--- src/main/java/io/github/josiasmartins/domain/entity/Produto.java
+++ src/main/java/io/github/josiasmartins/domain/entity/Produto.java
@@ -1,11 +1,19 @@
 package io.github.josiasmartins.domain.entity;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "produto")
 public class Produto {
 
+    @Id // key primary
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+    @Column(name = "descricao")
     private String descricao;
+    @Column(name = "preco_unitario")
     private BigDecimal preco;
 
     public Integer getId() {
