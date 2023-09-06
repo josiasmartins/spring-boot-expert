429dced188431b439696bc175cf96a136cead5c8
Caitano <josias.m.caitano@accenture.com>
2023-09-06 20:55:28 -0300

    feat: criando a classe de configuracao do Security

---
+++ src/main/java/io/github/josiasmartins/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package io.github.josiasmartins.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // implementa o @Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+}
